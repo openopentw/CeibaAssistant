6aa5dfadfd7b521d718e47c5dd2ecdf4cbc001be
flies1776 <flies1776@gmail.com>
2018-06-13 17:01:48 +0800

    main.py temp

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 import sys
-from helper_func import loginceiba
+#from helper_func import loginceiba
+sys.path.append('helper_func') #temp
+import loginceiba #temp
 
 cookie = loginceiba.info('b07902000', '*****', '1062')
 if cookie == 1:

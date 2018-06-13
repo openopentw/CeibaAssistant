de34be0a3a294e2cd15b54609f8f1e471c2627e2
RedBug312 <redbug312@gmail.com>
2018-06-13 14:50:22 +0800

    Avoid using sys.path.append to import libraries

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 import sys
-sys.path.append('helper_func')
-import loginceiba
+from helper_func import loginceiba
 
 login_result = loginceiba.info('b07902000', '*****', '1062')
 if login_result == 0:

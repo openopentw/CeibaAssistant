5ba6156684665f3fb17ee73be82af9e6b8c9fbdb
chouhy <b03902011@ntu.edu.tw>
2018-06-14 11:31:47 +0800

    for assemble test

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import sys
 #from helper_func import loginceiba
 sys.path.append('helper_func') #temp
 import loginceiba #temp
+import diff
+import notify
+import downloadfile
+from crawler import Crawler
 
 cookie = loginceiba.info('b07902000', '*****', '1062')
 if cookie == 1:
@@ -10,5 +14,10 @@ if cookie == 1:
 else:
     print('login_success, cookie:')
     print(cookie)
+    crawler = Crawler(cookie.strip('\n'))
+    courses = crawler.crawl()
+    #print(type(courses))
+    notifications,calendars ,downlaods = diff.diff(courses[0],[])
 
-
+    #diff.print_(downlaods[0])
+    downloadfile.downloadfile(downlaods)

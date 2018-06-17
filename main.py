ef2fd36fc956876ddd980d135b7663b19db207d9
RedBug312 <redbug312@gmail.com>
2018-06-17 20:11:35 +0800

    Read account info. from configuration file

--- main.py
+++ main.py
@@ -1,24 +1,39 @@
-import os
-import sys
+import argparse
+import configparser
+
 from diff import diff
 from helper_func import loginceiba
 from helper_func.notify import Notifier
 import downloadfile
 from crawler.crawler import Crawler
 
-cookie = loginceiba.info('b00000000', '****', '1062')
-if cookie == 1:
-    print("can't login!!")
-else:
+
+def main(config):
+    login_args = '{student} {password} {semester}'.format(**config['account'])
+    cookie = loginceiba.info(*login_args.split())
+
+    if cookie == 1:
+        print("can't login!!")
+        return
+
     print('login_success, cookie:')
     print(cookie)
+
     crawler = Crawler(cookie.strip('\n'))
     courses = crawler.crawl()
-    # print(type(courses))
+
     notifications, calendars, downloads = diff.diff(courses[0], [])
     notifier = Notifier()
     notifier.show_diff_notifications(notifications)
-
-    # for d in downlaods:
-    #     diff.print_(d)
     downloadfile.downloadfile(downloads)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Ceiba Assistant')
+    parser.add_argument('-c', '--config', action='store', metavar='config')
+    options = parser.parse_args()
+    options.config = options.config or 'config.ini'
+
+    config = configparser.ConfigParser()
+    config.read(options.config)
+    main(config)

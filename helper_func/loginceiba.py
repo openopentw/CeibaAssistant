c03a422c74424dd7907aa2a02621a5ce42b23080
RedBug312 <redbug312@gmail.com>
2018-06-20 00:26:22 +0800

    Add single quotes to argument of cd command

--- helper_func/loginceiba.py
+++ helper_func/loginceiba.py
@@ -2,7 +2,7 @@ import os
 
 def info(user, password, semester):
     root_dir = os.path.join(os.path.dirname(__file__), '..')
-    ret = os.system('cd {} && '.format(root_dir) + './helper_func/loginc ' + user + ' ' + password + ' ' + semester)
+    ret = os.system('cd \'{}\' && '.format(root_dir) + './helper_func/loginc ' + user + ' ' + password + ' ' + semester)
     if not ret == 0:
         return 1;
     else:
